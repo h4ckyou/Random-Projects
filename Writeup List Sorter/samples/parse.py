11bb759c02435e6756f141ab69ebcfb12c6f3608
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2024-06-27 15:23:56 +0100

    Create parse.py

---
+++ Writeup List Sorter/samples/parse.py
@@ -0,0 +1,17 @@
+"""
+@author: MarkUche
+@date: 27/06/2024
+"""
+
+import sys
+
+file_name = sys.argv[1]
+
+with open(file_name, "r") as f:
+    content = f.readlines()
+
+reversed_array = content[::-1]
+
+with open("modified.md", "w") as f:
+    for value in reversed_array:
+        f.write(value)
